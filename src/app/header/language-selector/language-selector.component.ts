68c83ca3550269032ea086a4845669d6305faf7d
mwvane <bzishvili57@gmail.com>
2023-03-26 10:14:36 +0400

    Implement language-selector component

--- src/app/header/language-selector/language-selector.component.ts
+++ src/app/header/language-selector/language-selector.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Helper } from 'src/app/helper';
+import { Language } from '../models/language';
 
 @Component({
   selector: 'app-language-selector',
   templateUrl: './language-selector.component.html',
-  styleUrls: ['./language-selector.component.css']
+  styleUrls: ['./language-selector.component.css'],
 })
 export class LanguageSelectorComponent {
+  data: Language[] = [
+    { id: 1, name: 'ENG' },
+    { id: 2, name: 'GEO' },
+    { id: 3, name: 'FRA' },
+  ];
+  selectedLanguage = this.data[0];
 
+  onChange() {
+    if (this.data) {
+      const index = Helper.getIndexById(this.selectedLanguage.id, this.data);
+      if (index >= 0) {
+        if (index + 1 < this.data.length) {
+          this.selectedLanguage = this.data[index + 1];
+        } else {
+          this.selectedLanguage = this.data[0];
+        }
+      }
+      console.log(`selected languaage: ${this.selectedLanguage.name}`)
+    }
+  }
 }
