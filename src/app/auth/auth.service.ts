31d33bc0875ec2f42dc9ae1215a0ed107508399c
mwvane <bzishvili57@gmail.com>
2023-03-27 00:31:35 +0400

    Craete auth.service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Form } from '@angular/forms';
+import { Result } from '../models/result';
+import { User } from './models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseUrl: string = 'https://localhost:7137/api/Auth/';
+
+  constructor(private http: HttpClient) {}
+  register(form: any) {
+    let user: User = {
+      email: form.value.email,
+      firstname: form.value.firstname,
+      lastname: form.value.lastname,
+      identityNumber: form.value.ID,
+      password: form.value.password,
+    };
+    return this.http.post<Result>(`${this.baseUrl}register`, user);
+  }
+
+  login(form: any) {
+    const user = {
+      username: form.value.username,
+      password: form.value.password,
+    };
+    return this.http.post<Result>(`${this.baseUrl}login`,user)
+  }
+}
