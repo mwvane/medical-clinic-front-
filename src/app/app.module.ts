f0127c5cac3c0c5126c0024e94c09285fbd5e68b
mwvane <bzishvili57@gmail.com>
2023-03-27 00:33:43 +0400

    Update imports in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header-component/header.component';
 import { NavbarComponent } from './header/navbar/navbar-component/navbar.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LanguageSelectorComponent } from './header/language-selector/language-selector.component';
 import { NavbarItemComponent } from './header/navbar/navbar-item/navbar-item.component';
 import { ButtonModule } from 'primeng/button';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DialogModule } from 'primeng/dialog';
+// import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -16,8 +22,20 @@ import { ButtonModule } from 'primeng/button';
     NavbarComponent,
     LanguageSelectorComponent,
     NavbarItemComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DialogModule,
+    // ToastModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
