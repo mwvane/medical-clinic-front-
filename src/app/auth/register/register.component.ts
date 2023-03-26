0bc340a53cb5385703d2073400dc12196a5294d0
mwvane <bzishvili57@gmail.com>
2023-03-27 00:34:47 +0400

    Update and bug fixes

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Validations } from '../validations';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm:any = new FormGroup({})
+  registerForm: any = new FormGroup({
+    firstname: new FormControl(null, [Validators.minLength(5)]),
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    ID: new FormControl(null, [Validators.minLength(11)]),
+    lastname: new FormControl(),
+    code: new FormControl(),
+    password: new FormControl(),
+  });
+
+  isFormValid: boolean = true;
+
+  constructor(private authService: AuthService) {}
+  onSubmit() {
+    console.log(this.registerForm.value);
+    this.authService.register(this.registerForm).subscribe((data) => {
+      console.log(data.res);
+    });
+  }
+
+  get firstnameValidation() {
+    return this.registerForm.get('firstname');
+  }
+
+  get isEmailValid() {
+    let valid = Validations.isEmailValid(this.registerForm.value.email);
+
+    return valid;
+  }
+
+  get isIdentityNumberValid() {
+    const valid = Validations.isIdentityNumberValid(this.registerForm.value.ID);
+    return valid;
+  }
+
+  get isPasswordValid() {
+    const valid = Validations.isPasswordValid(this.registerForm.value.password);
+    return valid;
+  }
 }
--- src/app/header/header-component/header.component.ts
+++ src/app/header/header-component/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalService } from 'src/app/modals/modal.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +10,13 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   isLoggedIn: boolean = false
 
+  constructor(private router: Router, private modalService: ModalService){}
+
   onAvatar(){
     console.log("clicked on avatar")
+  }
+
+  onLogin(){
+    this.modalService.loginModal = true
   }
 }
