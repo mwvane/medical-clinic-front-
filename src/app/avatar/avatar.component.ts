f1406cd3284981a4fa6361125e4092c3cea7cfd2
mwvane <bzishvili57@gmail.com>
2023-03-28 01:17:21 +0400

    Implement avater component

---
+++ src/app/avatar/avatar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.css'],
+})
+export class AvatarComponent {
+  @Input() name: string = 'ექიმი';
+  @Input() image: string = 'assets/doctorDefault.png';
+}
