94e20c78905e15ee50e9709c2aeed9239060aa37
mwvane <bzishvili57@gmail.com>
2023-03-28 01:18:15 +0400

    Create docotor model

---
+++ src/app/doctor/models/doctor.ts
@@ -0,0 +1,11 @@
+export interface Doctor {
+  id?: number;
+  name: string;
+  category: string[];
+  isPinned: boolean;
+  rating: number;
+  ID?: number;
+  email?: string;
+  booked?: number;
+  view: number;
+}
