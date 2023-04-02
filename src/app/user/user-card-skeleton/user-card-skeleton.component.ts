9797e0c3c9b8a7173c0f8bdf72ecfc676b7ea0c1
mwvane <bzishvili57@gmail.com>
2023-04-02 12:14:00 +0400

    Create user-card-skeleton

---
+++ src/app/user/user-card-skeleton/user-card-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card-skeleton',
+  templateUrl: './user-card-skeleton.component.html',
+  styleUrls: ['./user-card-skeleton.component.css']
+})
+export class UserCardSkeletonComponent {
+
+}
