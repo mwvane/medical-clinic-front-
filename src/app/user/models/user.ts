6a3e327dd210e814aee4d2cbf7dd11eb5cefe63d
mwvane <bzishvili57@gmail.com>
2023-04-02 12:15:19 +0400

    Create  user model

---
+++ src/app/user/models/user.ts
@@ -0,0 +1,12 @@
+
+export interface User {
+  id?: number;
+  firstname: string;
+  lastname: string;
+  ID?: string;
+  email?: string;
+  image?: string;
+  role: string;
+  bookCount?: number;
+
+}
