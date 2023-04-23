81eba9c475f9636787a42a94e1733d9fe789b2f4
mwvane <bzishvili57@gmail.com>
2023-04-24 01:39:29 +0400

    Update and bug fixies for forget passwprd

--- src/app/auth/forget-password/forget-password.component.ts
+++ src/app/auth/forget-password/forget-password.component.ts
@@ -2,35 +2,25 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ModalService } from 'src/app/modals/modal.service';
 import { Validations } from '../validations';
-import { Result } from 'src/app/models/result';
-import { MessageService } from 'primeng/api';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-forget-password',
   templateUrl: './forget-password.component.html',
   styleUrls: ['./forget-password.component.css'],
-  providers: [MessageService],
 })
 export class ForgetPasswordComponent {
-  isCodeSent: boolean = false;
-  canSentMail: boolean = true;
-  verificationStatusIcon: string = 'pi pi-check';
   verificationStatus: string = '';
   isPasswordTouched = false;
 
-  emailVerifyStatus: Result = {};
-
   form: FormGroup = new FormGroup({
-    email: new FormControl(),
-    code: new FormControl(),
     newPassword: new FormControl(),
     confirmPassword: new FormControl(),
+    email: new FormControl()
   });
 
   constructor(
     public modalService: ModalService,
-    private messageService: MessageService,
     private authService: AuthService
   ) {}
 
@@ -44,6 +34,7 @@ export class ForgetPasswordComponent {
         this.form.value.confirmPassword
       )
       .subscribe((data) => {
+        debugger
         if (data.res) {
           alert('Password changed');
           this.modalService.forgetPasswordModal = false;
@@ -53,61 +44,12 @@ export class ForgetPasswordComponent {
       });
   }
 
-  onEmail() {
-    const emailTo = this.form.value.email;
-    if (Validations.isEmailValid(emailTo)) {
-      this.authService.sendEmail(emailTo).subscribe((data) => {
-        debugger;
-        if (data) {
-          this.messageService.add({
-            severity: 'success',
-            summary: 'Email',
-            detail: 'იმეილი გაიგზავნა წარმატებით',
-            life: 3000,
-          });
-          this.isCodeSent = true;
-          this.canSentMail = false;
-        }
-      });
-    }
-  }
-
-  onVerification() {
-    if (!this.verificationStatus) {
-      this.verificationStatus = 'loading';
-      this.verificationStatusIcon = 'pi pi pi-check';
-      this.authService
-        .confirmEmail({
-          email: this.form.value.email,
-          code: this.form.value.code,
-        })
-        .subscribe((data) => {
-          this.emailVerifyStatus = data;
-          if (data.res) {
-            this.verificationStatus = 'done';
-            this.verificationStatusIcon = 'pi pi-check-circle';
-          } else {
-            this.verificationStatus = '';
-            this.verificationStatusIcon = 'pi pi-check';
-          }
-        });
-    }
-  }
-
-  get emailVerifyValidation() {
-    let field = this.form.controls['code'];
-    if (field.touched) {
-      if (this.emailVerifyStatus) {
-        if (this.emailVerifyStatus.res) {
-          field.setErrors(null);
-          return true;
-        } else {
-          field.setErrors({ incorect: true });
-          return this.emailVerifyStatus.errors;
-        }
-      }
-    }
-    return true;
+  onVerifyDone(email: string) {
+    this.verificationStatus = 'done';
+    this.form.patchValue({
+      email,
+    });
+    debugger
   }
 
   get isPasswordValid() {
